f46ebea9b0410fcfadde2b0a6b5d921ebd9594d4
zenovy shatzov <zshatzov@gmail.com>
2016-04-27 11:07:44 -0400

    Modified due to previous changes

--- snmp/SNMPv1AsyncSet.groovy
+++ snmp/SNMPv1AsyncSet.groovy
@@ -2,7 +2,7 @@ import com.avocent.dsview.net.snmp.*
 import com.avocent.dsview.net.snmp.impl.*
 
 
-vb = new SnmpSetVariableBinding('1.3.6.1.2.1.1.9.1.3.1', 'Hello World', SnmpSetVariableBinding.VariableType.OctetString)
+vb = new SnmpInputVariableBinding('1.3.6.1.2.1.1.9.1.3.1', 'Hello World', SnmpInputVariableBinding.VariableType.OctetString)
 request = new SnmpSetV1RequestBinding(1, 'demo.snmplabs.com', vb, 'private')
 nms = new Snmp4JNetworkManagementStation()
 response = nms.setSnmpV1Async({it.forEach{response->
@@ -16,5 +16,5 @@ response = nms.setSnmpV1Async({it.forEach{response->
             println it.variableTypeTextual
         }
     }
-  }
+}
 }, [request].stream())
